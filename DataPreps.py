3071799b61cc58c47a058a4cb51f9c741b90b3b6
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2018-08-27 14:40:57 +0530

    Preprocessing almost completed

--- DataPreps.py
+++ DataPreps.py
@@ -4,12 +4,7 @@ import pandas as pd
 import numpy as np
 
 
-def ModelGenerator(data_in, data_out):
-    df = pd.read_csv('../')
-    ourModel = GradientBoostingClassifier(learning_rate=0.001, n_estimators=1000)
-    ourModel.fit(data_in, data_out)
 
-    return ourModel
 
 def pre_process_BMI_category(bmi):
     if bmi < 15:
@@ -24,6 +19,25 @@ def pre_process_BMI_category(bmi):
         return "OW"
     else:
         return "OBESE"
+
+def ModelGenerator():
+    df_diabetes = pd.read_csv('../Diabetes-API/input/diabetes.csv')
+    df_train = df_diabetes[df_diabetes.columns[:-1]]
+    
+    df_train['BMICat'] = df_train['BMI'].apply(pre_process_BMI_category)
+
+    df_train = pd.concat([df_train, pd.get_dummies(df_train['BMICat'])], axis = 1)
+
+    df_train = df_train.drop(columns=['BMICat'], axis = 1)
+
+    # X,y
+    data_in = df_train
+    data_out = df_diabetes['Outcome']
+    
+    ourModel = GradientBoostingClassifier(learning_rate=0.001, n_estimators=1000)
+    ourModel.fit(data_in, data_out)
+
+    return ourModel
 
 # Lets assume that data enters in JSON dict form
 def PreProcess(InputDict):
@@ -43,5 +57,13 @@ def PreProcess(InputDict):
     df_train['BMICat'] = df_train['BMI'].apply(pre_process_BMI_category)
     df_train = pd.concat([df_train, pd.get_dummies(df_train['BMICat'])], axis = 1)
     df_train = df_train.drop(columns=['BMICat'], axis = 1)
+
+    toTrain = ourModel()
+
+    prediction = ourModel.predict(df_train)
+    prediction_probs = ourModel.prediction_probs(df_train)
+    return {"prediction" : prediction, "pred_0" : prediction_probs[0] * 100, "pred_1" : prediction_probs[1] * 100}
     
 
+#x = ModelGenerator()
+#print(x.feature_importances_)
